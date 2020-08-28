a54be0a6706d7f06cbc2b348f18db2dd165f3dda
Peterson Martins <petymn@hotmail.com>
2020-08-28 02:52:41 -0300

    Modification backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb+srv://peterson:peterson@omnistack.f7hkg.mongodb.net/se
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
