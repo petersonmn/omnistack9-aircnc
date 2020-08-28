5935faa842699502fad514428c161317d0197430
Peterson Martins <petymn@hotmail.com>
2020-08-28 03:40:13 -0300

    modification backend

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,56 @@
-import React from 'react';
+import React, { useState, useMemo} from 'react';
+
+import camera from '../../assets/camera.svg';
+
+import './styles.css';
 
 export default function Login(){
-    return <div />
+    const [thumbnail, setThumbnail] = useState(null);
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+   
+    // onde parei 7 min de video faltante
+    const preview = useMemo(() => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null;
+    },[thumbnail])
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label id="thumbnail" style={{ backgroundImage: `url(${preview})`}}>
+                <input type="file" onChange={event => setThumbnail(event.target.files[0])}/>
+                <img src={camera} alt="Select img"/>
+            </label>
+            <label htmlFor="company">EMPRESA</label>
+            <input
+                id="company"
+                placeholder="Sua empresa incrível"
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por virgula)</span></label>
+            <input
+                id="techs"
+                placeholder="Quais tecnologias usam?"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="price">VALOR DA DIARIA *<span>(em branco para GRATUITO)</span></label>
+            <input
+                id="price"
+                placeholder="Valor cobrado por dia?"
+                value={price}
+                onChange={event => setTechs(event.target.value)}
+            />
+
+            <button type="submit" className="btn">Cadastrar</button>
+
+        </form>
+    )
 }
